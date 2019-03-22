c97bc0bae57041a1ec40f36cb5dc9f55d7dffe63
Valentin Berlier <berlier.v@gmail.com>
2019-03-22 12:30:11 -0400

    Implement extractMath

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,68 @@ export interface Segment {
   value: string
 }
 
-export function extractMath(text: string): Segment[] {
-  return [
-    {
+export const SEGMENTS_REGEX = /(\\\$)|\$\$(.*?[^\\])\$\$|\$(.*?[^\\])\$/
+
+export function extractMath(input: string): Segment[] {
+  const segments: Segment[] = []
+
+  let dollar: string
+  let display: string
+  let inline: string
+
+  let [text, ...parts] = input.split(SEGMENTS_REGEX)
+
+  pushText(segments, text)
+
+  while (parts.length > 0) {
+    ;[dollar, display, inline, ...parts] = parts
+
+    if (dollar) {
+      pushText(segments, "$")
+    } else if (display) {
+      pushMath(segments, "display", display)
+    } else if (inline) {
+      pushMath(segments, "inline", inline)
+    }
+
+    ;[text, ...parts] = parts
+
+    pushText(segments, text)
+  }
+
+  return segments
+}
+
+function pushText(segments: Segment[], text: string) {
+  if (!text) {
+    return
+  }
+
+  const last = segments[segments.length - 1]
+
+  if (last && last.type === "text") {
+    last.value += text
+  } else {
+    segments.push({
       type: "text",
       math: false,
       value: text
-    }
-  ]
+    })
+  }
+}
+
+function pushMath(
+  segments: Segment[],
+  mode: "inline" | "display",
+  text: string
+) {
+  if (!text) {
+    return
+  }
+
+  segments.push({
+    type: mode,
+    math: true,
+    value: text.replace("\\$", "$")
+  })
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,13 +2,7 @@ import { extractMath } from "../src"
 
 test("empty input", () => {
   const segments = extractMath("")
-  expect(segments).toEqual([
-    {
-      type: "text",
-      math: false,
-      value: ""
-    }
-  ])
+  expect(segments).toEqual([])
 })
 
 test("plain text", () => {
@@ -18,6 +12,39 @@ test("plain text", () => {
       type: "text",
       math: false,
       value: "hello world"
+    }
+  ])
+})
+
+test("escaped dollar", () => {
+  const segments = extractMath("\\$")
+  expect(segments).toEqual([
+    {
+      type: "text",
+      math: false,
+      value: "$"
+    }
+  ])
+})
+
+test("inline math", () => {
+  const segments = extractMath("$123 + \\$1$")
+  expect(segments).toEqual([
+    {
+      type: "inline",
+      math: true,
+      value: "123 + $1"
+    }
+  ])
+})
+
+test("display math", () => {
+  const segments = extractMath("$$123 + \\$1$$")
+  expect(segments).toEqual([
+    {
+      type: "display",
+      math: true,
+      value: "123 + $1"
     }
   ])
 })
