02c515dba4025db5d1302b1422b8bc5c63f0b273
Valentin Berlier <berlier.v@gmail.com>
2019-03-22 11:15:09 -0400

    Setup typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export interface Segment {
+  type: 'text' | 'display' | 'inline';
+  math: boolean;
+  value: string;
+}
+
+export function extractMath(text: string): Segment[] {
+  return [
+    {
+      type: 'text',
+      math: false,
+      value: text
+    }
+  ]
+}
